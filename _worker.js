fe0a6ca6663e77a33e14ce164d85b39d1447d0df
ShinChan Ganteng <94566341+ShinChanYucan@users.noreply.github.com>
2023-12-03 08:42:59 +0700

    Add files via upload

--- _worker.js
+++ _worker.js
@@ -6,7 +6,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 
-const proxyIPs = ['cdn-b100.xn--b6gac.eu.org', '163.181.19.163'];
+const proxyIPs = ['cdn-b100.xn--b6gac.eu.org'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
