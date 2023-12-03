c68a0b705b674821c92bd36da0e811e9c4e3a579
ShinChan Ganteng <94566341+ShinChanYucan@users.noreply.github.com>
2023-12-03 07:59:44 +0700

    Add files via upload

--- _worker.js
+++ _worker.js
@@ -6,11 +6,13 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 
-const proxyIPs = ['163.181.19.163', '47.245.100.33'];
+const proxyIPs = ['163.181.19.163'];
 
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
-let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
+//let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
+
+let dohURL = 'https://cloudflare-dns.com/dns-query';
 
 // v2board api environment variables (optional) deprecated, please use planetscale.com instead
 
